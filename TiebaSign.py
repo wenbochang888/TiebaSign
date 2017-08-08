4dcdea141c636648e25781e740e585e8bb7bc299
wenbochang888 <727987105@qq.com>
2017-08-08 16:46:25 +0800

    2017年8月8日16:37:38

---
+++ TiebaSign.py
@@ -0,0 +1,71 @@
+import requests
+import re
+import time
+
+def getCookies(cookies):
+	for ck in cookies.split(";"):
+		name, value = ck.strip().split('=',1)
+		cookie[name] = value
+	return cookie
+
+def getMore():
+	url = "http://tieba.baidu.com/mo/"
+	html = requests.get(url = url, headers = headers, cookies = cookie).content.decode("utf-8")
+	pa = re.compile("\"([^\"]+tab=favorite)\"")
+	hrefList = pa.findall(html)
+	# print(html)
+	for href in hrefList:
+		# print("http://tieba.baidu.com" + href.replace("&amp;", "&"))
+		return "http://tieba.baidu.com" + href.replace("&amp;", "&")
+
+def getLike():
+	url = getMore()
+	html = requests.get(url = url, headers = headers, cookies = cookie).content.decode("utf-8")
+	pa = re.compile("kw.+?\">")
+	hrefList = pa.findall(html)
+	for href in hrefList:
+		# print(url[:-21] + href[:-2])
+		signList.append(url[:-21] + href[:-2])
+
+def getTitle(url):
+	html = requests.get(url = url, headers = headers, cookies = cookie).content.decode("utf-8")
+	pa = re.compile("<title.+<.title>")
+	titleList = pa.findall(html)
+	for title in titleList:
+		return title[7:-13]
+
+def Sign():
+	getLike()
+	for url in signList:
+		title = getTitle(url)
+		html = requests.get(url = url, headers = headers, cookies = cookie).content.decode("utf-8")
+		pa = re.compile("mo\/.+?\">签到")
+		hrefList = pa.findall(html)
+		if (len(hrefList) == 0):
+			with open("tieba.log", 'a+') as w:
+				w.write(title + "    已经签到\n")
+			continue
+		for href in hrefList:
+			setSign(title, "http://tieba.baidu.com/" + href.replace("&amp;","&")[:-4])
+
+def setSign(title, url):
+	html = requests.get(url = url, headers = headers, cookies = cookie)
+	with open("tieba.log", 'a+') as w:
+		w.write(title + "    在" + time.ctime() + "    签到成功\n")
+
+
+with open("tieba.log", 'a+') as w:
+	w.write("程序开始运行时间  " + time.ctime() + "\n\n")
+signList = []
+cookie = {}
+with open("cookies.txt", "r") as f:
+    cookies = f.read()
+cookie = getCookies(cookies)
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 UBrowser/6.1.2107.204 Safari/537.36"
+}
+Sign()
+with open("tieba.log", 'a+') as w:
+	w.write("\n\n程序结束时间  " + time.ctime() + "\n\n")
+
+
